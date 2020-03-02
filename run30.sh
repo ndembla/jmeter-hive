0822e9798a381886e524842c08b477803cd16beb
EC2 Default User <ec2-user@ip-172-31-56-149.us-west-2.compute.internal>
2020-03-02 23:44:21 +0000

    Changes for DWX run

---
+++ run30.sh
@@ -0,0 +1,3 @@
+#export PATH=/home/ec2-user/apache-jmeter-5.2.1/bin:$PATH
+export _JAVA_OPTIONS="-Djava.awt.headless=true -Xmx8192m"
+/home/ec2-user/apache-jmeter-5.2.1/bin/jmeter -n -t llap-test-30.jmx
